4ccfdd8703e067c14b678ec6b7e81121cfa01d99
Arek <54510093-arekrad@users.noreply.replit.com>
2026-03-11 03:39:21 +0000

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
